dd45c66559ab9f19776a24e8a9538a9deb43a44e
Digi-Duck <64693397+Digi-Duck@users.noreply.github.com>
2021-12-03 16:53:03 +0800

    day1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function (){
+    return 'hahaha';
+});
+
+Route::get('/news/{number?}',function ($number = null){
+    dd($number);
+    return 'hahaha';
+});
